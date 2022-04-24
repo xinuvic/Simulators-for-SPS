652af5da1c46327dc54cd31efecd1f0aa5ff0d7f
xinuvic <104274964+xinuvic@users.noreply.github.com>
2022-04-23 23:45:05 -0700

    Distance calculation between any two stations

---
+++ Distance.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  2 11:13:05 2021
+@author: CSUST
+"""
+import math
+def Distance(i,j,VehicleLocation):
+    distance = math.sqrt((VehicleLocation[i][0]-VehicleLocation[j][0])**2 + (VehicleLocation[i][1]-VehicleLocation[j][1])**2)
+    return distance
