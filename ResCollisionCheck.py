1856c8952f2f69f6f875abb0f9a8df0823347df8
xinuvic <104274964+xinuvic@users.noreply.github.com>
2022-04-24 15:10:39 -0700

    Update ResCollisionCheck.py

--- ResCollisionCheck.py
+++ ResCollisionCheck.py
@@ -2,6 +2,13 @@
 """
 Created on Thu Sep  2 11:15:25 2021
 @author: XIN
+Input: 
+i: Transmitter ID
+j: Another transmitter ID
+R: Resource selection list for all vehicles
+
+Output:
+Whether i and j select the same resources, 1--Yes, 0--No
 """
 def ResCollisionCheck(i,j,R):
     if R[i] == R[j] :
