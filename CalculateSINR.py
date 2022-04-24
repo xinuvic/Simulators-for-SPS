ebeee589d6b486d540f5b51faa1ee8caa617e98a
xinuvic <104274964+xinuvic@users.noreply.github.com>
2022-04-24 01:18:08 -0700

    Update CalculateSINR.py

--- CalculateSINR.py
+++ CalculateSINR.py
@@ -2,7 +2,15 @@
 """
 Created on Thu Sep  2 11:13:46 2021
 @author: XIN
+Input:
+i: Transmitter ID
+j: Receiver ID
+R: Resource selection list for all vehicles
+VehicleNum: Number of vehicles
+VehicleLocation: Location distribution of all vehicles
+Power: Transmit Power
 """
+
 from ResCollisionCheck import ResCollisionCheck
 from Distance import Distance
 def CalculateSINR(i,j,R,VehicleNum,VehicleLocation,Power):
