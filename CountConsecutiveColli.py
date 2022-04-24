387afed2e33c4e248fc6ba7b22a6c1154252d0a2
xinuvic <104274964+xinuvic@users.noreply.github.com>
2022-04-24 15:23:54 -0700

    Update CountConsecutiveColli.py

--- CountConsecutiveColli.py
+++ CountConsecutiveColli.py
@@ -11,12 +11,12 @@ StartTime: the starting point for simulation result recording
 maximalTime: Maximal tolerable time of access failure
 
 Output:
-CountSucceed,CountFail,collision,s
+CountSuccess,CountFail,collision,s
 """
 
 def CountConsecutiveNumber(Alist,number,timespot,SimulationTime,BeaconRate,RC,StartTime,maximalTime):
 
-    CountSucceed=0
+    CountSuccess=0
     CountFail=0
     collision=0
     s=0
@@ -25,13 +25,12 @@ def CountConsecutiveNumber(Alist,number,timespot,SimulationTime,BeaconRate,RC,St
         for j in range(0,len(Alist)):
             s+=1
             if Alist[j][t]==0:
-                CountSucceed+=1
+                CountSuccess+=1
             else:
                 collision+=1
-            # if RC set for last resource selection is no less than 20, regard it as collision
                 if t<=SimulationTime-BeaconRate:
                     if sum(Alist[j][t:int(t+maximalTime)])>=maximalTime: 
                         CountFail+=1
                     else:
-                        CountSucceed+=1
-    return CountSucceed,CountFail,collision,s
+                        CountSuccess+=1
+    return CountSuccess,CountFail,collision,s
