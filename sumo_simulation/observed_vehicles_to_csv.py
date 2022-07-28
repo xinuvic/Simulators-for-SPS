9c5cd451bd3c84388d572d139edf36de0e4b4c87
xinuvic <104274964+xinuvic@users.noreply.github.com>
2022-07-27 23:17:14 -0700

    Add files via upload

---
+++ sumo_simulation/observed_vehicles_to_csv.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  5 14:48:14 2022
+
+@author: CSUST
+"""
+
+import pandas as pd
+import csv
+
+
+
+set_of_staying_nodes=[]
+
+vehicle_data=[] 
+for s in range(1,401):
+    step_number=100+s
+    location_step=pd.read_csv("data\\location_for_timestep%s.csv"%str(step_number),header=None)
+    vehicle_data.append(location_step)
+    if s==1:  # list all vehicle IDs at first time-step
+        set_of_staying_nodes=list(vehicle_data[0][0])
+    else:  # check those non-repeated ones and delete them
+        for g in set_of_staying_nodes:
+            if g not in list(vehicle_data[s-1][0]):
+                set_of_staying_nodes.remove(g) 
+        
+vehicle_num = len(set_of_staying_nodes)   
+
+  
+# =============================================================================
+# using observe_vehicles as a list to record all locations          
+# =============================================================================
+observe_vehicles=[]
+for s in range(0,200):
+    for i in range(0,len(vehicle_data[s][0])):
+        if vehicle_data[s][0][i] in set_of_staying_nodes:
+            observe_vehicles.append([vehicle_data[s][1][i],vehicle_data[s][2][i]]) # save locations of all vehicles
+            
+
+# =============================================================================
+# save the locations of all vehicles during a given time period
+# =============================================================================
+filename='sumo_vehicle_location'
+n=0
+
+f=open('%s.csv'%filename,'w',newline='')
+writer=csv.writer(f)
+for i in observe_vehicles:
+    writer.writerow(i)
+f.close()
+
---
+++ sumo_simulation/two_way_traci.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 29 11:23:54 2022
+
+@author: xin
+"""
+
+
+import os, sys
+import traci
+import numpy as np
+import csv
+
+
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+    
+# =============================================================================
+# change the path based on your configuration
+# =============================================================================
+sumoBinary = r"D:\Eclipse\Sumo\bin\sumo-gui.exe"
+sumoCmd = [sumoBinary, "-c", "autobahn.sumocfg"]
+
+    
+
+def vehicle_information():
+    step = 0
+    a = 0
+    index = 0
+    x_coordinate = np.array([])
+    y_coordinate = np.array([])
+    
+    
+    while step < 1000: 
+       location_list=[]
+       traci.simulationStep()
+       for vehicleId in traci.vehicle.getIDList():
+           x, y = traci.vehicle.getPosition(vehicleId)           
+           x_coordinate = round(x,2)
+           y_coordinate = round(y,2)
+           location_list.append([vehicleId,x_coordinate,y_coordinate]) # saving three items in each line
+           index +=1       
+       if 100<step<=500: # sampling time duration
+           filename_locations='data\\location_for_timestep'+str(step) # each csv-file for each step
+           f = open('%s.csv'%filename_locations,'w',newline='')
+           writer = csv.writer(f)
+           for i in location_list:
+               writer.writerow(i)
+           f.close()
+       
+       vehicle_number = len(traci.vehicle.getIDList())
+       print('step',step,"Total number of vehicles: " + str(vehicle_number))
+       print()
+       a += 1             
+       step += 1
+       index = 0
+       x_coordinate = np.array([])
+       y_coordinate = np.array([])
+
+def main():    
+    traci.start(sumoCmd)
+    vehicle_information()
+    traci.close()
+
+       
+if __name__ == "__main__":
+    main()
+    
+
